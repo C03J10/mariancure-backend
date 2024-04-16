79530fa2dd2874c3b0748c0caf5862171d938ea5
C03J10 <cte_220000001593@uic.edu.ph>
2024-04-16 13:23:46 +0800

    add url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/mariancuredb"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://mcadmin:mariancure_db@152.42.214.45:3306/mariancuredb"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo=True
